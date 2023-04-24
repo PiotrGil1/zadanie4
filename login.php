806d48f4fac3ed76cc5efbf6f1b22d6b9ce71000
PiotrGil1 <130742010+PiotrGil1@users.noreply.github.com>
2023-04-24 13:17:41 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,45 @@
+<?php
+
+//login: admin
+//haslo: test
+
+
+include('config.php');
+session_start();
+$_SESSION['msg']='';
+
+if(!isset($_POST['log'])){
+    $_SESSION['msg'] = '';     
+}
+else{
+    if(empty($_POST['username']) || empty($_POST['password'])){
+        $_SESSION['msg'] = 'brak nazwy użytkownika lub hasła';
+        header('location: index.php');
+    }
+    else{
+        $haslo_hash = hash('sha512', $_POST['password']);
+
+        $sql = "SELECT username, password FROM users WHERE username = ? AND password = ?";
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param('ss', $_POST['username'], $haslo_hash);
+        $stmt->execute();
+        $wynik = $stmt->get_result();
+        $znalezione = $wynik->num_rows;
+        
+        if($znalezione>0){
+            
+            $_SESSION['islog'] = TRUE;
+            $_SESSION['username'] = $_POST['username'];
+            header('location: index.php');
+                   
+        }
+        else{
+            $_SESSION['msg'] = 'niepoprawna nazwa użytkownika lub hasło ';
+            header('location: index.php');
+        }
+
+    } 
+}
+
+?>
+
